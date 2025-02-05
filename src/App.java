a08303d8bdbcec05e258cf89288b473830c0ae96
wonK7 <poociki@gmail.com>
2025-02-04 23:20:52 -0800

    modified App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
-//Printable Interface – Defines a print() method that must be implemented.
-//Box<T> Generic Class – Stores a value of any type (T) and implements Printable.
-//App Class (Main Method) – Creates Box<Integer> and Box<String> objects and prints their values.
+// Printable Interface – Defines a print() method that must be implemented.
+// Box<T> Generic Class – Stores a value of any type (T) and implements Printable.
+// App Class (Main Method) – Creates Box<Integer> and Box<String> objects and prints their values.
 
 // interface determination
 interface Printable {
@@ -8,7 +8,7 @@ interface Printable {
 }
 
 // generic class
-class Box<T> implements Printable {
+class Box<T extends Comparable<T>> implements Printable {
     private T value;
 
     public Box(T value) {
@@ -23,15 +23,28 @@ class Box<T> implements Printable {
     public void print() {
         System.out.println("Value: " + value);
     }
-}
+
+    // Added a function to compare two Box objects.
+    public int compareTo(Box<T> other) {
+        return this.value.compareTo(other.value);
+    }
+    
+    // this.value < other.value  -1  The current object is smaller.
+    // this.value == other.value  0  Both values are equal.
+    // this.value > other.value  1  The current object is greater.
+}  // ✅ Box 클래스 닫는 중괄호
 
 // main class
 public class App {
     public static void main(String[] args) {
         Box<Integer> intBox = new Box<>(100);
-        Box<String> strBox = new Box<>("Hello, Java!");
+        Box<Integer> intBox2 = new Box<>(200);
 
         intBox.print();
-        strBox.print();
+        intBox2.print();
+
+        System.out.println("Comparison result: " + intBox.compareTo(intBox2));
     }
-}
+}  
+
+
